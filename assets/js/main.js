8b2a84919c0250740974492c29def73567d0f9a8
Nima <nimatf790@gmail.com>
2018-09-22 08:20:11 +0330

    Upload All

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 800);
+          return false;
+        }
+      }
+    });
+  });
+$(function() {
+      var pull    = $('#pull');
+        menu    = $('nav ul');
+        menuHeight  = menu.height();
+
+      $(pull).on('click', function(e) {
+        e.preventDefault();
+        menu.slideToggle();
+      });
+
+      $(window).resize(function(){
+            var w = $(window).width();
+            if(w > 320 && menu.is(':hidden')) {
+              menu.removeAttr('style');
+            }
+        });
+    });
+
+
+            $(window).scroll(function() {
+                if ($(document).scrollTop() > 100) {
+                    $('.navbar').addClass('after-scroll');
+                }
+                else {
+                    $('.navbar').removeClass('after-scroll'); 
+                }
+            });
